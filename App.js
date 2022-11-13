c9758ec49e612cee219003b96395794629d0fd2d
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-11-13 13:54:35 +0000

    basic style added

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello! Guys</Text>
+      </View>
+      <Text style={styles.dummyText}>Excuse me</Text>
+      <Button title="Tap dat!"></Button>
     </View>
   );
 }
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "green",
+    padding: 10,
   },
 });
